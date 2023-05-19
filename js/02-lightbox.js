3472a971fb63a32bae4dd74e4e3c205b71560d5d
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-05-19 16:02:31 +0300

    2-task-comleted

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// ==========
+const markup = galleryItems
+  .map((item) => `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></a></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+// ==========
+
+new SimpleLightbox('.gallery__link', {
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
