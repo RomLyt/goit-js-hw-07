0dea680827c95e36faf0ce5fcf0f581b405d208e
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-05-19 14:32:10 +0300

    1-task-completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery');
 
-console.log(gallery)
-
+// ==========
 const markup = galleryItems
-  .map((item) => `<li><img class="gallery__image" src=${item.preview} alt=${item.description}></li>`)
+  .map((item) => `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"></a></li>`)
   .join("");
 
-console.log(markup)
-
 gallery.insertAdjacentHTML("beforeend", markup);
+// ==========
+
+gallery.addEventListener('click', showLargeImage);
+
+function showLargeImage(evt) {
+    evt.preventDefault();
+
+    if(evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`)
+    instance.show()
+
+    gallery.addEventListener('keydown', (evt) => {
+        if(evt.code === "Escape") {
+            instance.close();
+        }
+    });
+}
+
